f5936140b34c0328baaa267a931f66bb8ff880aa
Nazar Tymtsias <nazar.tymtsias@gmail.com>
2022-09-09 15:32:00 +0200

    mutable

---
+++ Listsand Tuples/mutable.py
@@ -0,0 +1,14 @@
+
+
+# Documentation https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range
+shopping_list = ["milk",
+                "cookies"]
+another_list = shopping_list
+
+# print(id(another_list))
+# print(id(shopping_list))
+
+dict = {}
+
+dict.help()
+
